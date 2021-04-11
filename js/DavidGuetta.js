4b85b279c2d9472d8674ef4e3882bfcffe843e36
nguyenminhtuan37 <marshmallowstuan@gmail.com>
2021-04-11 11:08:53 +0700

    11/4 update

--- js/DavidGuetta.js
+++ js/DavidGuetta.js
@@ -11,14 +11,19 @@ innerTopsongs.addEventListener('mouseout', () => {
 
 var Titanium = document.getElementById("Titanium")
 var SongChanges = document.getElementById("song-change")
+var playImg = document.getElementById("play-img")
 
 SongChanges.addEventListener("click", PlayandPause)
+
 function PlayandPause(){
     if(Titanium.paused){
        Titanium.play()
+       playImg.src = "../img/pause button.png"
     }
     else{
         Titanium.pause()
+        playImg.src = "../img/play-button (24)_5ab94929cf6aa.png"
+        
     }
 }
 
